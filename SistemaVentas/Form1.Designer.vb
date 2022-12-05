f4f489f84d73db76f149632ccdf0e632f8d4cd36
Ferito23 <Ferito23@DESKTOP-JDPK6NL>
2022-12-05 00:47:20 -0500

    Agregar archivos de proyecto.

---
+++ SistemaVentas/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtusuario = New System.Windows.Forms.TextBox()
+        Me.txtcontraseña = New System.Windows.Forms.TextBox()
+        Me.btniniciar = New System.Windows.Forms.Button()
+        Me.btnsalir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblcontraseña = New System.Windows.Forms.Label()
+        Me.lblusuario = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtusuario
+        '
+        Me.txtusuario.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.txtusuario.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtusuario.Location = New System.Drawing.Point(101, 104)
+        Me.txtusuario.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtusuario.Name = "txtusuario"
+        Me.txtusuario.Size = New System.Drawing.Size(231, 37)
+        Me.txtusuario.TabIndex = 0
+        '
+        'txtcontraseña
+        '
+        Me.txtcontraseña.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtcontraseña.Location = New System.Drawing.Point(101, 166)
+        Me.txtcontraseña.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtcontraseña.Name = "txtcontraseña"
+        Me.txtcontraseña.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
+        Me.txtcontraseña.Size = New System.Drawing.Size(231, 37)
+        Me.txtcontraseña.TabIndex = 1
+        '
+        'btniniciar
+        '
+        Me.btniniciar.BackColor = System.Drawing.Color.Crimson
+        Me.btniniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btniniciar.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btniniciar.ForeColor = System.Drawing.Color.White
+        Me.btniniciar.Location = New System.Drawing.Point(50, 250)
+        Me.btniniciar.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btniniciar.Name = "btniniciar"
+        Me.btniniciar.Size = New System.Drawing.Size(153, 47)
+        Me.btniniciar.TabIndex = 3
+        Me.btniniciar.Text = "Ingresar"
+        Me.btniniciar.UseVisualStyleBackColor = False
+        '
+        'btnsalir
+        '
+        Me.btnsalir.BackColor = System.Drawing.Color.Crimson
+        Me.btnsalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnsalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnsalir.ForeColor = System.Drawing.Color.White
+        Me.btnsalir.Location = New System.Drawing.Point(216, 250)
+        Me.btnsalir.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnsalir.Name = "btnsalir"
+        Me.btnsalir.Size = New System.Drawing.Size(137, 47)
+        Me.btnsalir.TabIndex = 3
+        Me.btnsalir.Text = "Salir"
+        Me.btnsalir.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Crimson
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(399, 69)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Inicio de Sesion"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Crimson
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Label2.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label2.ImageAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Label2.Location = New System.Drawing.Point(0, 344)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(399, 49)
+        Me.Label2.TabIndex = 7
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        '
+        'lblcontraseña
+        '
+        Me.lblcontraseña.AutoSize = True
+        Me.lblcontraseña.BackColor = System.Drawing.Color.Transparent
+        Me.lblcontraseña.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblcontraseña.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.lblcontraseña.Location = New System.Drawing.Point(111, 169)
+        Me.lblcontraseña.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblcontraseña.Name = "lblcontraseña"
+        Me.lblcontraseña.Size = New System.Drawing.Size(155, 31)
+        Me.lblcontraseña.TabIndex = 8
+        Me.lblcontraseña.Text = "Contraseña"
+        '
+        'lblusuario
+        '
+        Me.lblusuario.AutoSize = True
+        Me.lblusuario.BackColor = System.Drawing.Color.Transparent
+        Me.lblusuario.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblusuario.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.lblusuario.Location = New System.Drawing.Point(111, 107)
+        Me.lblusuario.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblusuario.Name = "lblusuario"
+        Me.lblusuario.Size = New System.Drawing.Size(108, 31)
+        Me.lblusuario.TabIndex = 8
+        Me.lblusuario.Text = "Usuario"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(399, 393)
+        Me.Controls.Add(Me.lblcontraseña)
+        Me.Controls.Add(Me.lblusuario)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnsalir)
+        Me.Controls.Add(Me.btniniciar)
+        Me.Controls.Add(Me.txtcontraseña)
+        Me.Controls.Add(Me.txtusuario)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtusuario As System.Windows.Forms.TextBox
+    Friend WithEvents txtcontraseña As System.Windows.Forms.TextBox
+    Friend WithEvents btniniciar As System.Windows.Forms.Button
+    Friend WithEvents btnsalir As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblcontraseña As Label
+    Friend WithEvents lblusuario As Label
+End Class
---
+++ SistemaVentas/Reportes/FComprobante.vb
@@ -0,0 +1,14 @@
+﻿Public Class FComprobante
+    Public idventa As Integer
+    Private Sub FComprobante_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'TODO: esta línea de código carga datos en la tabla 'datosventasDataSet.GenerarComprobante' Puede moverla o quitarla según sea necesario.
+        Me.GenerarComprobanteTableAdapter.Fill(Me.datosventasDataSet.GenerarComprobante, idventa:=idventa)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+
+    Private Sub ReportViewer1_Load(sender As Object, e As EventArgs) Handles ReportViewer1.Load
+
+    End Sub
+End Class
